44d7337d25075e05177aa4e04e2da63b076c9560
Chris Kinney <chkinney@microsoft.com>
2025-08-26 20:32:56 -0700

    Remove references to directories in azsync file

--- src/cli/blobs.rs
+++ src/cli/blobs.rs
@@ -7,14 +7,13 @@ use crate::cli::{AzureStorageOptions, SyncOptions};
 /// Options for synchronizing files.
 #[derive(Clone, Debug, Args)]
 pub struct SyncFileOptions {
-    /// The file or directory to sync.
+    /// The file to sync.
     pub path: PathBuf,
 
     /// The name of the remote blob.
     ///
-    /// If not provided, the name of the file or directory being synchronized is
-    /// used as the blob name instead. Directories will be suffixed depending on
-    /// the archive type chosen.
+    /// If not provided, the name of the file being synchronized is used as the
+    /// blob name instead.
     #[arg(long)]
     pub blob_name: Option<String>,
 
