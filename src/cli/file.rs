7994f49ed78277bf0852f2b3526d14a5a2bf813e
Chris Kinney <chkinney@microsoft.com>
2025-08-27 02:19:53 -0700

    Support multiple files for azsync file

--- src/cli/file.rs
+++ src/cli/file.rs
@@ -7,15 +7,19 @@ use crate::cli::{AzureStorageOptions, SyncOptions};
 /// Options for synchronizing files.
 #[derive(Clone, Debug, Args)]
 pub struct SyncFileOptions {
-    /// The file to sync.
-    pub path: PathBuf,
+    /// The files to sync.
+    pub paths: Vec<PathBuf>,
 
-    /// The name of the remote blob.
-    ///
-    /// If not provided, the name of the file being synchronized is used as the
-    /// blob name instead.
-    #[arg(long)]
-    pub blob_name: Option<String>,
+    // NOTE: clap doesn't format doc comments correctly for long help yet:
+    // https://github.com/clap-rs/clap/issues/5900
+    #[doc = include_str!("file.blob_name.txt")]
+    #[arg(
+        long,
+        default_value = "#name#",
+        help = "The name of the remote blob.",
+        long_help = include_str!("file.blob_name.txt"),
+    )]
+    pub blob_name: String,
 
     /// Options for configuring how to synchronize with Azure.
     #[command(flatten)]
