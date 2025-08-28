bda0d6f003782603c5d0e05e47a04ad5683d1c2d
Chris Kinney <chkinney@microsoft.com>
2025-08-27 21:23:01 -0700

    Require minimum number of paths + docs update

--- src/cli/file.rs
+++ src/cli/file.rs
@@ -8,6 +8,25 @@ use crate::cli::{AzureStorageOptions, SyncOptions};
 #[derive(Clone, Debug, Args)]
 pub struct SyncFileOptions {
     /// The files to sync.
+    ///
+    /// NOTE ON GLOBBING (*.json):
+    ///
+    /// On most shells, globs are expanded by the shell. For example, the path
+    /// *.json will usually be converted by your shell into a list of all files
+    /// matching that pattern (like foo.json bar.json). When shells expand these
+    /// patterns, THEY ONLY INCLUDE FILES ON YOUR SYSTEM. Applications do not
+    /// receive the pattern. They only receive the list of paths that matched it
+    /// so they have no way of knowing what the pattern was.
+    ///
+    /// As a result, if you want to synchronize a file that does not exist on
+    /// your system, you MUST specify it literally. For example, if you want to
+    /// pull the file foo.json, you MUST specify foo.json because *.json will
+    /// not be expanded by your shell to include it.
+    ///
+    /// There is currently no way to pull all files matching a pattern from the
+    /// remote storage. IF YOU WANT TO SYNCHRONIZE A DIRECTORY, ARCHIVE IT
+    /// FIRST. You can synchronize foos.zip easily because it is only one file.
+    #[arg(required = true, num_args = 1..)]
     pub paths: Vec<PathBuf>,
 
     // NOTE: clap doesn't format doc comments correctly for long help yet:
